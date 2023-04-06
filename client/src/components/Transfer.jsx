f56765f0db4e2731baa561cfa2947233c8533e0b
fribas84 <fribas84.gmail.com>
2023-04-06 17:45:09 -0300

    updated doc format

--- client/src/components/Transfer.jsx
+++ client/src/components/Transfer.jsx
@@ -1,53 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import server from "../server";
 
-function Transfer({ address, setBalance }) {
+function Transfer({ address, setBalance, addresses }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [destOptions, setDestOptions] = useState([]);
 
+  useEffect(() => {
+    const removeOrigin = () => {
+      return addresses.filter(add => add !== address);
+    }
+    
+    const createOptions = () =>{}
+    setDestOptions(removeOrigin());
+  },
+    [address]);
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
   async function transfer(evt) {
-    evt.preventDefault();
-
-    try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
-    } catch (ex) {
-      alert(ex.response.data.message);
-    }
+  evt.preventDefault();
+  try {
+    const {
+      data: { balance },
+    } = await server.post(`send`, {
+      sender: address,
+      amount: parseInt(sendAmount),
+      recipient,
+    });
+    setBalance(balance);
+  } catch (ex) {
+    alert(ex.response.data.message);
   }
+}
 
-  return (
-    <form className="container transfer" onSubmit={transfer}>
-      <h1>Send Transaction</h1>
+return (
+  <form className="container transfer" onSubmit={transfer}>
+    <h1>Send Transaction</h1>
 
-      <label>
-        Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
-      </label>
+    <label>
+      Send Amount
+      <input
+        placeholder="1, 2, 3..."
+        value={sendAmount}
+        onChange={setValue(setSendAmount)}
+      ></input>
+    </label>
 
-      <label>
-        Recipient
-        <input
-          placeholder="Type an address, for example: 0x2"
-          value={recipient}
-          onChange={setValue(setRecipient)}
-        ></input>
-      </label>
+    <div>
+      Recipient
+      <div className="select-dropdown">
+        <select id="" onChange={e => setRecipient(e.target.value)} value={recipient}>
+          <option value=""> -- Select a destination address address -- </option>
+            {
+              destOptions.map((val)=> <option value={val}>{val}</option>
 
-      <input type="submit" className="button" value="Transfer" />
-    </form>
+              )
+            }
+        </select>
+      </div>
+    </div>
+
+    <input type="submit" className="button" value="Transfer" />
+  </form>
   );
 }
 
