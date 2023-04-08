85ff7cc0605b71e79bff8112f664037ff13974d6
fribas84 <fribas84.gmail.com>
2023-04-08 10:57:17 -0300

    finish modal desing

--- client/src/components/ModalSign.jsx
+++ client/src/components/ModalSign.jsx
@@ -18,9 +18,30 @@ const ModalSign = ({
   sendAmount,
 }) => {
   const [privateKey, setPrivateKey] = useState("");
-  console.log("showModal: ", showModal);
+  console.log(privateKey);
 
   const sing = () =>{
+
+    const txtData = {
+        sender: address,
+        amount: parseInt(sendAmount),
+        recipient: recipient
+    }
+
+    
+      // try {
+      //   const {
+      //     data: { balance },
+      //   } = await server.post(`send`, {
+      //     sender: address,
+      //     amount: parseInt(sendAmount),
+      //     recipient,
+      //   });
+      //   setBalance(balance);
+      // } catch (ex) {
+      //   alert(ex.response.data.message);
+      // }
+
 
   }
   const setValue = (setter) => (evt) => setter(evt.target.value);
@@ -42,7 +63,32 @@ const ModalSign = ({
           ></input>
         </label>
 
-        <input type="submit" className="button" value="Sign" />
+        <label>
+          Origin Address:
+          <input
+            disabled={true}
+            value={address}
+          ></input>
+        </label>
+        <label>
+          Destination Address:
+          <input
+            disabled={true}
+            value={recipient}
+          ></input>
+        </label>
+
+        <label>
+          Amount:
+          <input
+            disabled={true}
+            value={sendAmount}
+          ></input>
+        </label>
+
+
+
+        <input type="submit" className="button" value="Sign and Send" />
         <button className="button cancel" onClick={() => setShowModal(false)}>Cancel</button>
       </form>
       
