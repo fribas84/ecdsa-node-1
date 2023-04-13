fcaef0e4b8564caf7eb602ca00c7e0901e156b44
fribas84 <fribas84.gmail.com>
2023-04-13 14:52:57 -0300

    removed unused api

--- server/index.js
+++ server/index.js
@@ -28,21 +28,6 @@ app.get("/balances", (req,res)=>{
 
 })
 
-app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
-
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
-
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
-  }
-});
-
 app.post("/transfer", (req, res) => {
   const { address, txData,txDataHashed, signature, recoverBit } = req.body;
   const amount = txData.amount;
